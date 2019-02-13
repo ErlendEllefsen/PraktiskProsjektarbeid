8980b5695733e525d9c2c2eb70822a3b6107eea5
Erlend Ellefsen <ekbe_97@hotmail.no>
2019-02-13 10:48:18 +0100

    Update main.js

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -1,3 +1,9 @@
+/* Main JS Doucument */
+
+
+
+//Splitscreen
+
 function splitScroll() {
 	const controller = new ScrollMagic.Controller();
 
@@ -6,8 +12,48 @@ function splitScroll() {
 		triggerElement:'.about-title',
 		triggerHook: 0
 	})
+
 	.setPin('.about-title')
 	.addTo(controller);
 }
 
 splitScroll();
+
+
+//Appeareffect
+
+function scollAppear1(){
+	var introText = document.querySelector('.intro-text-1');
+	var introPosition = introText.getBoundingClientRect().top;
+	var screenPosition = window.innerHeight / 2;
+
+	if (introPosition < screenPosition) {
+		introText.classList.add('intro-appear');
+	}
+}
+
+	window.addEventListener('scroll',scollAppear1);
+
+function scollAppear2(){
+	var introText = document.querySelector('.intro-text-2');
+	var introPosition = introText.getBoundingClientRect().top;
+	var screenPosition = window.innerHeight / 2;
+
+	if (introPosition < screenPosition) {
+		introText.classList.add('intro-appear');
+	}
+}
+
+	window.addEventListener('scroll',scollAppear2);
+
+function scollAppear3(){
+	var introText = document.querySelector('.intro-text-3');
+	var introPosition = introText.getBoundingClientRect().top;
+	var screenPosition = window.innerHeight / 2;
+
+	if (introPosition < screenPosition) {
+		introText.classList.add('intro-appear');
+	}
+}
+
+	window.addEventListener('scroll',scollAppear3);
