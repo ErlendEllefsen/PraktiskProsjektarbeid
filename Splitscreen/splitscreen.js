f1725914256272d4cef88ce595836c739a60cd81
VikingsCreed <ekbe_97@hotmail.no>
2019-03-14 11:34:36 +0100

    t

--- Splitscreen/splitscreen.js
+++ Splitscreen/splitscreen.js
@@ -12,3 +12,14 @@ function splitScroll() {
 }
 
 splitScroll();
+
+
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 3000,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
