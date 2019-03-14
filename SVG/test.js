d33816eb4160605c130ce85eaf702d3744d5a65f
VikingsCreed <ekbe_97@hotmail.no>
2019-03-14 11:40:18 +0100

    end

--- SVG/test.js
+++ SVG/test.js
@@ -1,17 +1,14 @@
 
-function splitScroll() {
-var lineDrawing = anime({
-  targets: '#lineDrawing .lines path',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 3000,
-  delay: function(el, i) { return i * 250 },
-  direction: 'alternate',
-  loop: true
-});
-}
 
-splitScroll();
+var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 4000,
+    loop: false
+});
+
+
 
 
 /*<script id="rendered-js">
@@ -24,4 +21,4 @@ splitScroll();
   direction: 'alternate',
   loop: true });
       //# sourceURL=pen.js
-</script>
+</script>*/
