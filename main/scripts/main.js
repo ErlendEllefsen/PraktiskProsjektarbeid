d1aab4514ae93d2f1f04a43b67f607fe5555968c
VikingsCreed <ekbe_97@hotmail.no>
2019-03-26 13:07:18 +0100

    hehehe

--- main/scripts/main.js
+++ main/scripts/main.js
@@ -112,24 +112,52 @@ function drawMap(){
 
  function scollAppear(){
 	
-	var introText = document.querySelector('.intro-text-1');
+	var introText1 = document.querySelector('.intro-text-1');
 	var introText2 = document.querySelector('.intro-text-2');
 	var introText3 = document.querySelector('.intro-text-3');
 	var introText4 = document.querySelector('.intro-text-4');	
 	var introText5 = document.querySelector('.intro-text-5');
 	var introText6 = document.querySelector('.intro-text-6');
+	var introText7 = document.querySelector('.intro-text-7');
+	var introText8 = document.querySelector('.intro-text-8');
+	var introText9 = document.querySelector('.intro-text-9');
+	var introText10 = document.querySelector('.intro-text-10');	
+	var introText11 = document.querySelector('.intro-text-11');
+	var introText12 = document.querySelector('.intro-text-12');
+	var introText13 = document.querySelector('.intro-text-13');
+	var introText14 = document.querySelector('.intro-text-14');
+	var introText15 = document.querySelector('.intro-text-15');
+	var introText16 = document.querySelector('.intro-text-16');	
+	var introText17 = document.querySelector('.intro-text-17');
+	var introText18 = document.querySelector('.intro-text-18');
+	var introText19 = document.querySelector('.intro-text-19');
+	var introText20 = document.querySelector('.intro-text-20');
 
-	var introPosition = introText.getBoundingClientRect().top;
+	var introPosition1 = introText1.getBoundingClientRect().top;
 	var introPosition2 = introText2.getBoundingClientRect().top;
 	var introPosition3 = introText3.getBoundingClientRect().top;
 	var introPosition4 = introText4.getBoundingClientRect().top;
 	var introPosition5 = introText5.getBoundingClientRect().top;
 	var introPosition6 = introText6.getBoundingClientRect().top;
+	var introPosition7 = introText7.getBoundingClientRect().top;
+	var introPosition8 = introText8.getBoundingClientRect().top;
+	var introPosition9 = introText9.getBoundingClientRect().top;
+	var introPosition10 = introText10.getBoundingClientRect().top;
+	var introPosition11 = introText11.getBoundingClientRect().top;
+	var introPosition12 = introText12.getBoundingClientRect().top;
+	var introPosition13 = introText13.getBoundingClientRect().top;
+	var introPosition14 = introText14.getBoundingClientRect().top;
+	var introPosition15 = introText15.getBoundingClientRect().top;
+	var introPosition16 = introText16.getBoundingClientRect().top;
+	var introPosition17 = introText17.getBoundingClientRect().top;
+	var introPosition18 = introText18.getBoundingClientRect().top;
+	var introPosition19 = introText19.getBoundingClientRect().top;
+	var introPosition20 = introText20.getBoundingClientRect().top;
 
 	var screenPosition = window.innerHeight / 1.5;
 
-	if (introPosition < screenPosition)
-		introText.classList.add('intro-appear');
+	if (introPosition1 < screenPosition)
+		introText1.classList.add('intro-appear');
 	if (introPosition2 < screenPosition)
 		introText2.classList.add('intro-appear');
 	if (introPosition3 < screenPosition)
@@ -140,6 +168,35 @@ function drawMap(){
 		introText5.classList.add('intro-appear');
 	if (introPosition6 < screenPosition)
 		introText6.classList.add('intro-appear');
+	if (introPosition7 < screenPosition)
+		introText7.classList.add('intro-appear');
+	if (introPosition8 < screenPosition)
+		introText8.classList.add('intro-appear');
+	if (introPosition9 < screenPosition)
+		introText9.classList.add('intro-appear');
+	if (introPosition10 < screenPosition)
+		introText10.classList.add('intro-appear');
+	if (introPosition11 < screenPosition)
+		introText11.classList.add('intro-appear');
+	if (introPosition12 < screenPosition)
+		introText12.classList.add('intro-appear');
+	if (introPosition13 < screenPosition)
+		introText13.classList.add('intro-appear');
+	if (introPosition14 < screenPosition)
+		introText14.classList.add('intro-appear');
+	if (introPosition15 < screenPosition)
+		introText15.classList.add('intro-appear');
+	if (introPosition16 < screenPosition)
+		introText16.classList.add('intro-appear');
+	if (introPosition17 < screenPosition)
+		introText17.classList.add('intro-appear');
+	if (introPosition18 < screenPosition)
+		introText18.classList.add('intro-appear');
+	if (introPosition19 < screenPosition)
+		introText19.classList.add('intro-appear');
+	if (introPosition20 < screenPosition)
+		introText20.classList.add('intro-appear');
+
 
 }
 	window.addEventListener('scroll',scollAppear);
